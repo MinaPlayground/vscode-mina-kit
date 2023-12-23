4dcb7001d6bfa7cfe9c08e5246de24a107a6b61a
devarend <devarend@gmail.com>
2023-12-23 23:27:13 +0100

    added terminal, created mina kit main screen

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,86 +1,27 @@
-import 'xterm/css/xterm.css';
-import {useEffect, useRef} from 'react'
-import {WebContainer} from "@webcontainer/api";
-import {Terminal} from 'xterm'
-import {FitAddon} from 'xterm-addon-fit';
-
-interface VsCodeApi {
-    postMessage(message: any): void;
-
-    setState(state: any): void;
-
-    getState(): any;
-}
-
-declare const acquireVsCodeApi: () => VsCodeApi;
-
 function App() {
-    const webcontainerInstance = useRef<any>();
-
-    useEffect(() => {
-        const vscode = acquireVsCodeApi();
-
-        window.addEventListener('message', event => {
-            const message = event.data;
-            switch (message.command) {
-                case 'updateFile':
-                    webcontainerInstance.current.fs.writeFile(message.path, message.value)
-                    break;
-                case 'loadFiles':
-                    (async () => {
-                        const terminalEl = document.querySelector('.terminal');
-                        const terminal = new Terminal({
-                            convertEol: true,
-                        });
-                        const fitAddon = new FitAddon();
-                        terminal.loadAddon(fitAddon);
-                        terminal.open(terminalEl as HTMLElement);
-                        fitAddon.fit();
-                        webcontainerInstance.current = await WebContainer.boot();
-                        const bootFiles = message.files
-                        await webcontainerInstance.current.mount(bootFiles);
-
-                        webcontainerInstance.current.on("server-ready", (port, url) => {
-                            vscode.postMessage({command: 'preview', text: url})
-                        });
-
-                        const shellProcess = await webcontainerInstance.current.spawn('jsh');
-
-                        const xtermResizeOb = new ResizeObserver(function (entries) {
-                            fitAddon.fit();
-                            shellProcess.resize({
-                                cols: terminal.cols,
-                                rows: terminal.rows,
-                            });
-                        });
-
-                        xtermResizeOb.observe(terminalEl);
-
-                        shellProcess.output.pipeTo(
-                            new WritableStream({
-                                write(data) {
-                                    terminal.write(data);
-                                },
-                            })
-                        );
-
-                        const input = shellProcess.input.getWriter();
-                        terminal.onData((data) => {
-                            input.write(data);
-                        });
-                    })();
-                    break;
-            }
-        });
-        vscode.postMessage({command: 'initialize'})
-    }, []);
-
     return (
-        <>
-            <div className="flex h-screen">
-                <div className="terminal h-full w-full"/>
+        <section>
+            <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-200 md:text-5xl lg:text-6xl">Mina kit</h1>
+                <p className="mb-8 text-lg font-normal text-gray-400 lg:text-xl sm:px-16 lg:px-48">The All-in-One Mina Protocol extension to run and deploy zkApps/Smart Contracts.</p>
+                <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+                    <a href="#"
+                       className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 focus:ring-4 focus:ring-blue-300">
+                        Initialize terminal
+                        <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true"
+                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                    <a href="#"
+                       className="inline-flex justify-center items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-gray-400 rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-black focus:ring-4 focus:ring-gray-100">
+                        Deploy Smart Contract
+                    </a>
+                </div>
             </div>
-        </>
+        </section>
+
     );
 }
 
---
+++ web-ui/src/Terminal.tsx
@@ -0,0 +1,87 @@
+import 'xterm/css/xterm.css';
+import {useEffect, useRef} from 'react'
+import {WebContainer} from "@webcontainer/api";
+import {Terminal} from 'xterm'
+import {FitAddon} from 'xterm-addon-fit';
+
+interface VsCodeApi {
+    postMessage(message: any): void;
+
+    setState(state: any): void;
+
+    getState(): any;
+}
+
+declare const acquireVsCodeApi: () => VsCodeApi;
+
+function App() {
+    const webcontainerInstance = useRef<any>();
+
+    useEffect(() => {
+        const vscode = acquireVsCodeApi();
+
+        window.addEventListener('message', event => {
+            const message = event.data;
+            switch (message.command) {
+                case 'updateFile':
+                    webcontainerInstance.current.fs.writeFile(message.path, message.value)
+                    break;
+                case 'loadFiles':
+                    (async () => {
+                        const terminalEl = document.querySelector('.terminal');
+                        const terminal = new Terminal({
+                            convertEol: true,
+                        });
+                        const fitAddon = new FitAddon();
+                        terminal.loadAddon(fitAddon);
+                        terminal.open(terminalEl as HTMLElement);
+                        fitAddon.fit();
+                        webcontainerInstance.current = await WebContainer.boot();
+                        const bootFiles = message.files
+                        await webcontainerInstance.current.mount(bootFiles);
+
+                        webcontainerInstance.current.on("server-ready", (port, url) => {
+                            vscode.postMessage({command: 'preview', text: url})
+                        });
+
+                        const shellProcess = await webcontainerInstance.current.spawn('jsh');
+
+                        const xtermResizeOb = new ResizeObserver(function (entries) {
+                            fitAddon.fit();
+                            shellProcess.resize({
+                                cols: terminal.cols,
+                                rows: terminal.rows,
+                            });
+                        });
+
+                        xtermResizeOb.observe(terminalEl);
+
+                        shellProcess.output.pipeTo(
+                            new WritableStream({
+                                write(data) {
+                                    terminal.write(data);
+                                },
+                            })
+                        );
+
+                        const input = shellProcess.input.getWriter();
+                        terminal.onData((data) => {
+                            input.write(data);
+                        });
+                    })();
+                    break;
+            }
+        });
+        vscode.postMessage({command: 'initialize'})
+    }, []);
+
+    return (
+        <>
+            <div className="flex h-screen">
+                <div className="terminal h-full w-full"/>
+            </div>
+        </>
+    );
+}
+
+export default App
